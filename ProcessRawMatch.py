0c14833094f8d8c9587bd423213ba438e5cf7df0
Proverbs-16-25 <jacobkapiniphiri@gmail.com>
2026-02-13 11:46:27 +0200

    deleted:    ProcessRawMatch.py

--- ProcessRawMatch.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("statarea_lean.db")
-cursor = conn.cursor()
-
-cursor.execute("SELECT stat_facts FROM raw_matches")
-rows = cursor.fetchall()
-
-conn.close()
